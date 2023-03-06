66e2489c7e36c7dc20b5cfd7c9d6a3537326aac5
Joseph Price <josephprice747@gmail.com>
2023-03-06 10:04:15 +0000

    added api for liverpool

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function getLocation() {
+    const API = `https://us1.locationiq.com/v1/search.php?key=pk.bb29feda391c78027fb7306d762a84ca&q=liverpool&format=json`;
+  }
+  return <div className="App"></div>;
 }
 
 export default App;
+
+// pk.bb29feda391c78027fb7306d762a84ca
