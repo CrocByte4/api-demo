b9698c54b8b55b2016e832b41fe9e6ae5da7b8d7
Joseph Price <josephprice747@gmail.com>
2023-03-06 10:52:40 +0000

    add button which retrieves lata and long location data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  function getLocation() {
+  const [location, setLocation] = useState({});
+
+  async function getLocation() {
     const API = `https://us1.locationiq.com/v1/search.php?key=pk.bb29feda391c78027fb7306d762a84ca&q=liverpool&format=json`;
+    const res = await axios.get(API);
+    console.log(res.data[0]);
+    setLocation(res.data[0]);
   }
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {location.display_name && (
+        <p>
+          {location.display_name} is at lat and lon: {location.lat} /
+          {location.lon}
+        </p>
+      )}
+      <button onClick={getLocation}>Get Data</button>
+    </div>
+  );
 }
 
 export default App;
